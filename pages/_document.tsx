fa2c9cd43a41c7f2183920854b075c92306bcdce
Sergey Polin <s.polin@innopolis.university>
2023-11-03 14:53:43 +0300

    add hash check

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,15 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      </Head>
       <body>
         <noscript className="flex w-full justify-center bg-red-700 p-8">
           You need to enable JavaScript to run this app.
         </noscript>
         <Main />
         <NextScript />
-        <Script strategy='beforeInteractive' src="https://telegram.org/js/telegram-web-app.js" />
       </body>
     </Html>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,19 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 import { useEffect } from "react";
-import useWebApp from "@/hooks/useWebApp";
+import useTelegramInitData from "@/hooks/useTelegramInitData";
 
 function Home() {
-  const webApp = useWebApp();
-
-  useEffect(() => {
-    if (webApp) {
-      console.log("webApp", webApp);
-    }
-  }, [webApp]);
+  const initData = useTelegramInitData();
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h1 className="text-center text-5xl font-bold">Hello world!</h1>
+      <div>
+        <p className="text-center text-3xl font-bold">Hash: {JSON.stringify(initData, null, 2)}</p>
+      </div>
     </main>
   );
 }
