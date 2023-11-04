efd7924bb8a916a60a2504e764a1017dc30b8f14
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-11-05 00:39:03 +0300

    feat: add target alias in tables

--- pages/api/stat.jsx
+++ pages/api/stat.jsx
@@ -1,36 +1,35 @@
 async function handler(req, res) {
-    const { stat_name, limit, offset } = req.query;
+  const { stat_name, limit, offset, target_alias } = req.query;
 
-    const authorizationToken = process.env.AUTH_TOKEN;
+  const authorizationToken = process.env.AUTH_TOKEN;
 
-    if (!authorizationToken) {
-        return res.status(500).json({ message: 'Authorization token is missing.' });
+  if (!authorizationToken) {
+    return res.status(500).json({ message: "Authorization token is missing." });
+  }
+
+  try {
+    const apiUrl = `https://62d6-188-130-155-146.ngrok-free.app/views/execute/${stat_name}?limit=${limit}&offset=${offset}&target_alias=${target_alias}`;
+
+    const resp = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + authorizationToken,
+      },
+    });
+
+    if (resp.ok) {
+      const data = await resp.json();
+      res.status(200).json(data);
+    } else {
+      console.log(await resp.json());
+      console.error(`Request failed with status ${resp.status}`);
+      res.status(resp.status).json({ message: "Error" });
     }
-
-    try {
-        const apiUrl = `https://62d6-188-130-155-146.ngrok-free.app/views/execute/${stat_name}?limit=${limit}&offset=${offset}`;
-
-        const resp = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'accept': 'application/json',
-                'Authorization': 'Bearer ' + authorizationToken
-            }
-        });
-
-        if (resp.ok) {
-            const data = await resp.json();
-            res.status(200).json(data);
-        } else {
-            console.log(await resp.json())
-            console.error(`Request failed with status ${resp.status}`);
-            res.status(resp.status).json({ message: 'Error' });
-        }
-
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ message: 'Error' });
-    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Error" });
+  }
 }
 
 export default handler;
